297ef2a4e9025813f838a8079f81e684c56c2073
mobasith <mobasith7@gmail.com>
2024-10-21 11:55:57 +0530

    fist commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { UsersModule } from './users/users.module';
       host: process.env.DB_HOST || 'localhost',
       port: 3306,
       username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
+      password: process.env.DB_PASSWORD || 'pass@word1',
       database: process.env.DB_NAME || 'sales',
       autoLoadEntities: true,
       synchronize: true,
